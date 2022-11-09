51ddc3582513e5a2971b1d3482647cc6ac806bc1
quique500 <some500.q@gmail.com>
2022-11-09 00:22:37 -0600

    cambio de ruta por default(con interrogante (10))

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -34,7 +34,13 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                //->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+                Route::middleware('web', 'auth')
+                ->prefix('')    //prefigo para no poner en todas las rutas la propiedad "auth"
+                //->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
